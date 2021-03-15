b801d222458ba1648912309feea65f053d99ce87
Dhruv-Pindawala <dhruv.pindawala@gmail.com>
2021-03-15 12:14:41 +0530

    used market stack api in my bot

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from twilio.rest import Client
 import os
+from marketstack import get_stock_price
 
 app = Flask(__name__)
 
@@ -12,8 +13,14 @@ TWILIO_NUMBER = 'whatsapp:+14155238886'
 
 def process_msg(msg):
     response = ""
-    if msg == "Hi":
-        response = "Hello master, I am your stock market bot. What can I do for you?"
+    if "hi" in msg.lower():
+        response = "Hello master, I am your stock market bot. I support 72 global exchanges. and 1,25,000+ stock tickers.\n\nHope I may help you.\n\n"
+        response += "Please type \"sym:<stock_symbol>\" to know the price of the stock."
+    elif "sym:" in msg.lower():
+        data = msg.split(":")
+        stock_symbol = data[1]
+        stock_data = get_stock_price(stock_symbol)
+        response = "=> Stock data of " + stock_symbol + " is:\n\n" + str(stock_data) + "\n\n (Note: Stock values are in USD.)"
     else:
         response = "Please type \"Hi\" to get started."
     return response
